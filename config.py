45ec4a1d5fd843ed925f8e2ced422873c5198056
Johannes Marko <147028901+trapperjoe@users.noreply.github.com>
2025-10-22 14:52:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-# fjgfdngfdi
+def func_01(P1):
+  print("Dies ist die Funktion func_01")
+  print("P1 = ", P1)
+  return
+
+def func_02(P2):
+  print("Dies ist die Funktion func_02")
+  print("P2 = ", P2)
+  return
+
+def func_03(P3):
+  print("Dies ist die Funktion func_03")
+  print("P3 = ", P3)
+  return
+
